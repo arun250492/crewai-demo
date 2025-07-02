2e4912278b51f527954846fd20fca9249e62f49f
arun250492 <56998209+arun250492@users.noreply.github.com>
2025-07-03 00:54:43 +0530

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,7 @@
+from crewai_tools import YoutubeVideoSearchTool
+
+# Initialize the tool for general YouTube video searches
+#yt_tool = YoutubeVideoSearchTool()
+# Initialize the tool with a specific Youtube channel handle to target your search
+yt_tool = YoutubeVideoSearchTool(youtube_channel_handle='@codebasics')
+
